89232d9e40a0ba544f46e76ca46a74b4da492e24
Kesya <kesyavlad74@gmail.com>
2022-12-22 00:23:39 +0200

    chang let

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let first = +prompt('Введіть преше число');
-let second = +prompt('Введіть друге число');
-let third = +prompt('Введіть третье число');
+const first = +prompt('Введіть преше число');
+const second = +prompt('Введіть друге число');
+const third = +prompt('Введіть третье число');
 
 if (!isNaN(first)&&!isNaN(second)&&!isNaN(third)){
-    let result = (first+second+third)/3
+    const result = (first+second+third)/3
     alert(`Середне значення: ${result}`)
 }else {
     alert('Треба вписувати тільки числа')
